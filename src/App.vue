c688cb666aae8d34022d8b294d87eaff4fe6cd2e
Konrad <kjimenez@softonitg.com>
2019-09-18 11:32:10 -0600

    Initial codebase

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class='card-content is-flex is-horizontal-center'>
+      <figure class="image is-128x128">
+        <img src="https://avatars2.githubusercontent.com/u/28398930?s=200&v=4">
+      </figure>
+    </div>
+    <div id="employees" v-for="employee in employees" :key="employee.id">
+      <Employee :name=employee.name :lname=employee.lname />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Employee from './components/Employee.vue'
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Employee,
+  },
+  data() {
+    return {
+      employees: [],
+    }
+  },
+  methods: {
+    fetchEmployees() {
+      axios.get('http://local.softon-apps.com:8000/employees')
+        .then(response => {
+          this.employees = response.data
+        })
+        .catch(() => {
+        })
+        .finally(() => {
+        })
+    }
+  },
+  mounted() {
+    this.fetchEmployees()
   }
 }
 </script>
@@ -24,5 +51,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.is-horizontal-center {
+  justify-content: center;
 }
 </style>
---
+++ src/components/Employee.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class='column'>
+    <div class='card equal-height'>
+      <div class='card-content is-flex-reverse is-horizontal-center'>
+        <div>
+          <h1><strong>Nombre:</strong> {{ name }}</h1><br/>
+        </div>
+        <div>
+          <h1><strong>Apellido:</strong> {{ lname }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Employee',
+  props: {
+    name: String,
+    lname: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  margin-top: 10px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
